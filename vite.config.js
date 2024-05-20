ad167c5ea39e0c0e22964a903dfd39db26bdddc2
mahan.dev <mahan.dev.front@gmail.com>
2024-05-20 22:19:18 +0330

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/billingApp",
-  server: {
-    fs: {
-      strict: false,
-    },
-    hmr: {
-      overlay: false,
-    },
-    index: '/BillingApp/index.html', 
-  },
-})
+  base: "https://mahan-dev.github.io/BillingApp",
+});
